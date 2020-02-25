03095e88068b200fbf4aaf779af043eba745be00
Danny Beyrer <beyrerdn@gmail.com>
2020-02-25 17:55:37 -0500

    Initial Commit

---
+++ ServiceProviders/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ServiceProviders
+//
+//  Created by Daniel Nathan Beyrer on 2/25/20.
+//  Copyright © 2020 Daniel Nathan Beyrer. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
